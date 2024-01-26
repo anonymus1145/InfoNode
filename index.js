5f9e13cdf020aefc468f690c25ebed48fcc1f1cb
Anonymus1145 <Anonymus1145@yahoo.com>
2024-01-26 12:00:47 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-const http = require('node:http');
-const fs = require('fs');
-const hostname = 'localhost';
-const port = 8080;
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// Create a server object
-const server = http.createServer((req, res) => {
-
-    // Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-
-    // Send an html file
-    if (req.url == '/') {
-        res.end(fs.readFileSync('./index.html'));
-        res.statusCode = 204;
-    } else if (req.url == '/about') {
-        res.end(fs.readFileSync('./about.html'));
-        res.statusCode = 204;
-    } else if (req.url == '/contact-me') {
-        res.end(fs.readFileSync('./contact-me.html'));
-        res.statusCode = 204;
-    } else {
-        res.statusCode = 404;
-        res.end(fs.readFileSync('./404.html'));
-    }
-    
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+})
