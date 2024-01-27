e1b9e4a71e828a16da2f11d79bb53b4ae9e1d4ae
Anonymus1145 <Anonymus1145@yahoo.com>
2024-01-27 18:49:13 +0200

    Add models

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Import the mongoose module first for respose reasons when connecting to the database
-const mongoose = require("mongoose");
+// Add .env files requirments
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -14,21 +14,17 @@ const app = express();
 
 //Configuration and connecting to the database -------------------------------------
 
-// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
-// Included because it removes preparatory warnings for Mongoose 7.
-// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+// Set up mongoose connection
+const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+// eslint-disable-next-line no-undef
+const mongoDB = process.env.CONNECTION_STRING;
 
-// Define the database URL to connect to.
-const mongoDB = "mongodb://127.0.0.1/my_database";
-
-// Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
 async function main() {
     await mongoose.connect(mongoDB);
-    mongoose.connection.once("open", (() => console.log("Database connected")));
 }
-
+/* This code creates the default connection to the database and reports any errors to the console. */
 // -------------------------------------------------------------------------------
 
 // view engine setup
@@ -48,13 +44,13 @@ app.use("/users", usersRouter);
 app.use("/about", aboutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
 // eslint-disable-next-line no-unused-vars
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
